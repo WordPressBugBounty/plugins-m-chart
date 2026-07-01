e0710f388066bb9f9c4249f90fc2237c56bfbdf7
Sean Murphy <sean@iamseanmurphy.com>
2026-07-01 03:24:54 +0000

    2.2

--- m-chart/components/admin-ui/index.asset.php
+++ m-chart/components/admin-ui/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1084ae56cc2113402f15');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cd16b283cad10202a8ce');
--- m-chart/components/block/index.asset.php
+++ m-chart/components/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '6c6f9f52dec3191847b0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7275751ea9ffd43da3ab');
